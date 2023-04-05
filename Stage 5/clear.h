184626625804acfad756496a4f26701e908093bc
jason-szeto <52977449+jason-szeto@users.noreply.github.com>
2023-04-05 01:47:53 -0600

    added comments

--- Stage 5/clear.h
+++ Stage 5/clear.h
@@ -1,5 +1,22 @@
+/*    
+Group Student Name: Jason Szeto, Ben Cacic
+Student Email: jszet006@mtroyal.ca, bcaci729@mtroyal.ca
+File Name: events.H
+Instructor: Paul Pospisil
+Assignment: Space Invaders - Stage 2
+Recent Update: None (Code was found in the I drive)
+*/
+
+
 #ifndef CLEAR_H
 #define CLEAR_H
+
+/* 
+Function name: clear_screen
+Input: void *base
+Output: clears the screen
+Function details: Mark's quick clear. 
+*/
 
 void clear_screen(void *base);
 
