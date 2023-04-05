9a0aed5d68b23dea78b8c6c82dcc5e44d2064e88
jason-szeto <52977449+jason-szeto@users.noreply.github.com>
2023-04-05 01:43:40 -0600

    commented stage 2

---
+++ Stage 2/bitmap.h
@@ -0,0 +1,30 @@
+/*    
+Group Student Name: Jason Szeto, Ben Cacic
+Student Email: jszet006@mtroyal.ca, bcaci729@mtroyal.ca
+File Name: splash.H
+Instructor: Paul Pospisil
+Assignment: Space Invaders - Stage 2
+Recent Update: April 1th, 2023
+
+Description: houses all the bitmaps that are used in the raster library
+*/
+
+#ifndef BITMAP_H
+#define BITMAP_H
+
+#include "types.h"
+
+
+extern UINT16 Cannon_Word[];
+
+extern UINT16 Alien_Begin_Word[];
+
+extern UINT16 Alien_Middle_Word[];
+
+extern UINT16 Alien_End_Word[];
+
+extern UINT16 laser_word[];
+
+extern UINT16 s_word[];
+
+#endif
